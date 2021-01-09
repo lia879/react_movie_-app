501e443ddfb454b613278371366d11e5608d8f74
Lia879 <54521457+lia879@users.noreply.github.com>
2021-01-08 18:43:04 -0600

    updating js files

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,10 +1,10 @@
-import React { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import Movie from './components/Movie';
+import Movie from "./components/Movie";
 
-const FEATURED_APi = "https://api.themoviedb.or/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-const IMAGES_API = "https://image.tmdb.org/t/p/w1280";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
@@ -13,23 +13,23 @@ function App() {
 
   const [movies, setMovies] = useState([]);
 
-  useEffect(async () => {
-    const movieResp = await fetch(FEATURED_API);
-    CONST movies = await moviesResp.json();
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
 
-    setMovies(movies);
 
-
-  }, [])
+  }, []);
 
 
   return <div>
-    {movies.map(movie => (
-      <Movie />
-    ))}
-
-  </div>;
-
+    {movies.length > 0 && movies.map((movie) =>
+      <Movie />)}</div>;
 }
+
+
 
 export default App;
